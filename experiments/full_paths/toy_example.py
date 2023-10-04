44fc8167702f3876fcf12048a5a2992ec8b012e2
Xavier Dupuis <xavier.dupuis@u-bourgogne.fr>
2023-10-04 10:33:57 +0200

    change tol parameters for toy example

--- experiments/full_paths/toy_example.py
+++ experiments/full_paths/toy_example.py
@@ -16,7 +16,7 @@ _ = full_path(X, y , Lambda, ratio=0., k_max=0., rtol_pattern=1e-6, atol_pattern
 _ = path_solver(X, y, Lambda, k_max=0., rtol_pattern=1e-6, atol_pattern = 1e-6, rtol_gamma=1e-6, split_max=1e1, log=0)
 
 # Full path
-Gamma, Sol, Primal, Gap, M, Split, T = full_path(X, y , Lambda, ratio=1., k_max=1e3, rtol_pattern=1e-6, atol_pattern = 1e-8, rtol_gamma=1e-12, split_max=1e1, log=1)
+Gamma, Sol, Primal, Gap, M, Split, T = full_path(X, y , Lambda, ratio=1., k_max=1e3, rtol_pattern=1e-8, atol_pattern = 1e-10, rtol_gamma=1e-10, split_max=1e1, log=1)
 
 # Solution path
 fig, ax = plt.subplots()
@@ -29,12 +29,12 @@ plt.show()
 
 # Full path with ridge regularization
 eps = np.sqrt(2e-4)
-Gamma2, Sol2, Primal2, Gap2, M2, Split2, T2 = full_path(np.concatenate((X, eps*np.eye(X.shape[-1]))), np.concatenate((y, np.zeros(X.shape[-1]))) , Lambda, ratio=1., k_max=1e3, rtol_pattern=1e-6, atol_pattern = 1e-8, rtol_gamma=1e-12, split_max=1e1, log=1)
+Gamma2, Sol2, Primal2, Gap2, M2, Split2, T2 = full_path(np.concatenate((X, eps*np.eye(X.shape[-1]))), np.concatenate((y, np.zeros(X.shape[-1]))) , Lambda, ratio=1., k_max=1e3, rtol_pattern=1e-8, atol_pattern = 1e-10, rtol_gamma=1e-10, split_max=1e1, log=1)
 print(f'\nwith no ridge regularization: nodes = {np.array(Gamma)}\n')
 print(f'with ridge regularization: nodes = {np.array(Gamma2)}\n')
 
 
 # Solver 
 gamma = 3.
-sol, (primal, gap) = path_solver(X, y , gamma*Lambda, k_max=1e3, rtol_pattern=1e-6, atol_pattern = 1e-8, rtol_gamma=1e-12, split_max=1e1, log=0)
+sol, (primal, gap) = path_solver(X, y , gamma*Lambda, k_max=1e3, rtol_pattern=1e-8, atol_pattern = 1e-10, rtol_gamma=1e-10, split_max=1e1, log=0)
 print(f'solution for gamma = {gamma}: {sol}')
