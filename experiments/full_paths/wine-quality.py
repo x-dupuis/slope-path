a714cbd2fca29c303a9716cd801295c231026f6c
Xavier Dupuis <xavier.dupuis@u-bourgogne.fr>
2023-10-13 17:11:00 +0200

    add full_path_LASSO

--- experiments/full_paths/wine-quality.py
+++ experiments/full_paths/wine-quality.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 from timeit import default_timer as timer
 
 sys.path.append('../..')
-from modules import full_path, path_solver, dual_sorted_L1_norm as dual_norm, pattern
+from modules import full_path, path_solver, dual_sorted_L1_norm as dual_norm, pattern, full_path_LASSO
 
 # Import of the data set
 wine = pd.read_csv('../datasets/winequality-red.csv', sep=';')
@@ -32,11 +32,12 @@ Lambda = np.sqrt(range(1,12))-np.sqrt(range(0,11))
 # Numba compilation
 _ = full_path(X, y , Lambda, ratio=0., k_max=0., rtol_pattern=1e-6, atol_pattern = 1e-6, rtol_gamma=1e-6, split_max=1e1, log=0)
 _ = path_solver(X, y, Lambda, k_max=0., rtol_pattern=1e-6, atol_pattern = 1e-6, rtol_gamma=1e-6, split_max=1e1, log=0)
+_ = full_path_LASSO(X, y, ratio=0., k_max=0., rtol_sign=1e-6, atol_sign = 1e-6, rtol_gamma=1e-6, split_max=1e1, log=0)
 
-# Full path
+# Full SLOPE path
 Gamma, Sol, Primal, Gap, M, Split, T = full_path(X, y , Lambda, ratio=1., k_max=1e4, rtol_pattern=1e-10, atol_pattern = 1e-10, rtol_gamma=1e-10, split_max=1e1, log=1)
 
-# Absolute solution path + ols
+# Absolute SLOPE solution path + ols
 abs_Sol = [np.abs(sol) for sol in Sol]
 
 ols = np.linalg.solve(X.T@X, X.T@y)
@@ -49,14 +50,35 @@ for gamma in Gamma:
 ax.axhline(0, color='k', linestyle=':', xmax=0.95)
 ax.plot(0, [np.abs(ols)], 'rx')
 # ax.set_xscale('symlog')
-ax.set_title('Absolute solution path')
+ax.set_title('Absolute SLOPE solution path')
 plt.show() 
 
-fig.savefig('../results/wine-quality_path', bbox_inches="tight", pad_inches=0.05)
+fig.savefig('../results/wine-quality_SLOPEpath', bbox_inches="tight", pad_inches=0.05)
 
-# Solver 
+# SLOPE solver 
 frac = 0.5; gamma = frac*dual_norm(X.T@y, Lambda)
 t_start = timer()
 sol, (primal, gap), k = path_solver(X, y , gamma*Lambda, k_max=1e3, rtol_pattern=1e-10, atol_pattern = 1e-10, rtol_gamma=1e-10, split_max=1e1, log=0)
 print(f'pattern for {frac} x gamma_max: {pattern(sol, tol=1e-10)}')
 print(f'elapsed time = {timer() - t_start:.2e}s, crossed nodes = {k}, primal-dual gap = {gap:.2e}')
+
+# Full LASSO path
+Gamma, Sol, S, Split, T = full_path_LASSO(X, y, ratio=1., k_max=1e4, rtol_sign=1e-10, atol_sign = 1e-10, rtol_gamma=1e-10, split_max=1e1, log=1)
+
+# Absolute LASSO solution path + ols
+abs_Sol = [np.abs(sol) for sol in Sol]
+
+ols = np.linalg.solve(X.T@X, X.T@y)
+abs_ols = np.abs(ols)
+    
+fig, ax = plt.subplots()
+ax.plot([1.1*Gamma[0]] + Gamma, [Sol[0]] + abs_Sol)
+for gamma in Gamma:
+    ax.axvline(gamma, color='k', linestyle=':')
+ax.axhline(0, color='k', linestyle=':', xmax=0.95)
+ax.plot(0, [np.abs(ols)], 'rx')
+# ax.set_xscale('symlog')
+ax.set_title('Absolute LASSO solution path')
+plt.show() 
+
+fig.savefig('../results/wine-quality_LASSOpath', bbox_inches="tight", pad_inches=0.05)
